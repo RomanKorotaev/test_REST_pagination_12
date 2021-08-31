b89564ec1030ee4576a2ddce27807b940bfe1809
Roman Korotaev <kro1986@ukr.net>
2021-08-31 17:46:18 +0300

    01

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 import axios from 'axios';
 
+var _ = require('lodash'); // добавляем эту запись, чтобы не выбивало ощшибку, при использовании функции _.throttle избиблиотеки Lodash
+
+
 const  refs  = {
     form: document.querySelector('#form'),
     input: document.querySelector('#search'),
-    container: document.querySelector('.container')
+    container: document.querySelector('.container'),
+    searchBtn2: document.querySelector('#searchBtn2'),
+    more: document.querySelector('#more')
 }
 
 // //  // Пример запроса на бекенд через функцию  fetch
@@ -19,7 +24,9 @@ const  refs  = {
 // }
 // //  //------------------------------------------------------------------------
 
+
  // Пример запроса на бекенд через функцию  библиотеку axios
+ // сначала установить ее через коменду npm install, а потом экспортируем в файл index.js: import axios from 'axios';
 const hndlerSubmit = (event) => {
     event.preventDefault()
     const value = refs.input.value;
@@ -29,6 +36,8 @@ const hndlerSubmit = (event) => {
     .then(result => renderCollection (result.data.drinks))
     .catch(err => console.log(err))        
 }
+
+
 
 function createItem({strDrinkThumb, strDrink}) {  
     const article = `<article>
@@ -56,4 +65,50 @@ function renderCollection(arr) {
 //     return coctailItem;
 // }
 
-refs.form.addEventListener ('submit', hndlerSubmit)
+refs.form.addEventListener('submit', hndlerSubmit)
+
+
+
+// ---------------------------Пример пагинации по API Github.--------- ПАГИНАЦИЯ
+
+//это счётчик, который увеличивается при каждом запросе
+let currentPage = 1;
+
+const gitHandlerSummit = (e) => {
+    e.preventDefault();
+    const value = refs.input.value;
+    axios.get (`https://api.github.com/search/users?q=${value}&client_id=f13c8683999b71a3600d&client_secret=eef562743c32c4ec5620bede29fc2b437c46f55c&page=${currentPage}`)
+        .then(result => renderGitCollection(result.data.items))
+        .then(() => currentPage++) // увеличиваем счётчик после каждого запроса
+    .catch( (err) => console.log(err))
+}
+
+
+const gitHandlerSummitScroll = (e) => {
+    console.log ("Сработала функция gitHandlerSummitScroll")
+    e.preventDefault();
+    const value = refs.input.value;
+    axios.get (`https://api.github.com/search/users?q=${value}&client_id=f13c8683999b71a3600d&client_secret=eef562743c32c4ec5620bede29fc2b437c46f55c&page=${currentPage}`)
+        .then(result => renderGitCollection(result.data.items))
+        .then(() => currentPage++) // увеличиваем счётчик после каждого запроса
+    .catch( (err) => console.log(err))
+}
+
+
+refs.searchBtn2.addEventListener('click', gitHandlerSummit)
+refs.more.addEventListener('click', gitHandlerSummit)
+window.addEventListener ('scroll', _.throttle(gitHandlerSummitScroll, 1000) )
+
+
+function createGitItem({avatar_url, login}) {  
+    const article = `<article>
+        <img src= '${avatar_url}'  alr= '${login}'/>
+        <p>${login}</p>
+        </article>
+    `
+    refs.container.insertAdjacentHTML('beforeend', article)
+}
+
+function renderGitCollection(arr) {
+    arr.forEach (el => createGitItem (el) )
+}
